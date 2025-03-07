2036a74a655886bbcd422fd3648da0375505ba10
“SYB” <259704084@qq.com>
2025-03-07 15:57:52 +0800

    back 3/3

--- ControlPanel_QT/main.cpp
+++ ControlPanel_QT/main.cpp
@@ -1,13 +1,10 @@
 #include "dialog.h"
-#include "Gyroscope3DWidget.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Dialog w;
-    Gyroscope3DWidget *gyroWidget = new Gyroscope3DWidget();
-    gyroWidget->show();
     w.show();
 
     return a.exec();
